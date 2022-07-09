d8f85496dd8aa08bed4a3f7424c50c2185b368f5
Connor <kor.connor@gmail.com>
2022-07-10 01:38:21 +0900

    서버 실행

--- main.js
+++ main.js
@@ -1,6 +1,14 @@
 // @ts-check
-console.log('Hello, world!')
 
-const someString = 'Hello'
-const result = Math.log(someString)
-console.log(result)
+const http = require('http')
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200
+    res.end('Hello!')
+})
+
+const PORT = 4000
+server.listen(PORT, () => {
+    console.log('The server is listening at port: ${PORT}.')
+})
+
