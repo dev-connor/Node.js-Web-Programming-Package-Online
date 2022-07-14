56f3fa8b26350526458fd3ff2b37ec2579d8b6d1
Connor <kor.connor@gmail.com>
2022-07-15 01:25:36 +0900

    Prototype 중간저장

--- src/main.js
+++ src/main.js
@@ -1,12 +1,26 @@
 // Prototype
 
-function Student(name) {
+function Person(name) {
     this.name = name
 }
-
-Student.prototype.greet = function greet() {
+Person.prototype.greet = function greet() {
     return `Hi, ${this.name}!`
 }
 
+function Student(name) {
+    this.__proto__.constructor(name)
+}
+
+Student.prototype.study = function study() {
+    return `${this.name} is studying.`
+}
+
+Object.setPrototypeOf(Student.prototype, Person.prototype)
+
 const me = new Student('Connor')
 console.log(me.greet())
+console.log(me.greet())
+
+const autherPerson = new Person('Foo')
+console.log(anotherPerson instanceof Student)
+console.log(anotherPerson instanceof Porson)
