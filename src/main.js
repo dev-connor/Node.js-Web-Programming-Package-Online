8a025988e63ac0c41f5aa1af6f4ccc82ae664c18
Connor <kor.connor@gmail.com>
2022-07-15 00:52:34 +0900

    Prototype

--- src/main.js
+++ src/main.js
@@ -1,20 +1,5 @@
-var numCounters = 0
+// Prototype
 
-function getCounter() {
-    numCounters += 1
-
-    var result = { count: count, total: 0 }
-    function count() {
-        result.total += 1
-    } 
-    return result
+function Student() {
+    this.name = name
 }
-
-var counterA = getCounter()
-counterA.count()
-counterA.count()
-  
-var counterB = getCounter()
-counterB.count()
-
-console.log(counterA.total, counterB.total, numCounters)
