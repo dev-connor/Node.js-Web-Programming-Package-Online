333b9765e6161c22b7bedb856056ce0f17f5deee
Connor <kor.connor@gmail.com>
2022-07-19 00:12:06 +0900

    algorithm2. 각 도시별로 개와 고양이를 키우는 사람의 수

--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
+const { stringify } = require("querystring")
+
 const people = [
     {
         age: 20,
@@ -7,7 +9,7 @@ const people = [
     {
         age: 20,
         city: '서울',
-        pet: ['cat', 'dog'],
+        pet: 'dog',
     },
     {
         age: 40,
@@ -56,3 +58,56 @@ function solveAModern() {
 
 console.log('solveA', solveA())
 console.log('solveAModern', solveAModern())
+
+// B. 각 도시별로 개와 고양이를 키우는 사람의 수
+
+/* 
+{
+    "서울": {
+        "dog": 2,
+        "cat": 1,
+    },
+    "대구": {
+        "dog": 1,
+        "cat": 1,
+    },
+    "부산": {
+        "cat": 1,
+    },
+}
+*/
+
+/** @typedof {Object.<string, Object<string, number>>} PetsOfCities */
+
+function solveB() {
+    /** @type {PetsOfCities} */
+    const result = {}
+
+    for (const person of people) {
+        const {city, pet: petOrPets} = person
+
+        if (petOrPets) {
+            const petsOfCity = result[city] || {}
+
+            if (typeof petOrPets === 'string') {
+                const pet = petOrPets
+
+                const origNumPetsOfCity = petsOfCity[pet] || 0
+                petsOfCity[pet] = origNumPetsOfCity + 1
+            } else {
+                for (const pet of petOrPets) {
+                    const origNumPetsOfCity = petsOfCity[pet] || 0
+                    petsOfCity[pet] = origNumPetsOfCity + 1
+
+                }
+                
+            }
+            result[city] = petsOfCity
+        }
+    }
+
+    return result
+
+}
+
+console.log('solveB', solveB())
