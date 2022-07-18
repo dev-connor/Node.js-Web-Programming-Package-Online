c8ece09cbbc7a4413f0071e3d63c3e79b4ff6ce5
Connor <kor.connor@gmail.com>
2022-07-18 21:05:52 +0900

    spread operator2

--- src/main.js
+++ src/main.js
@@ -3,3 +3,27 @@ const ary = [1, 2, 3, 4, 5]
 const [head, ...rest] = ary 
 
 console.log(head, ...rest)
+
+const personalData = {
+    email: 'abc@def.com',
+    password: '****'
+}
+
+const publicData = {
+    nickname: 'foo'
+}
+
+const overrides = {
+    email: 'fff@fff.com',
+}
+
+const user = {
+    ...personalData,
+    ...publicData,
+    ...overrides,
+}
+
+console.log(user)
+
+
+
