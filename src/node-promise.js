8193e32254ab2f194259b363a4b5d7d55f2f00cc
Connor <kor.connor@gmail.com>
2022-07-21 00:39:30 +0900

    promise API

--- src/node-promise.js
+++ src/node-promise.js
@@ -1,7 +1,24 @@
 // @ts-check
 
+const { rejects } = require('assert')
 const fs = require('fs')
 
-fs.readFile('.gitignore', 'utf-8', (error, value) => {
-    console.log(value)
-})
+/**
+ * 
+ * @param {string} fileName 
+ */
+function readFileInPromise(fileName) {
+    return new Promise((resolve, reject) => {
+        fs.readFile('.gitignore', 'utf-8', (error, value) => {
+            if (error) {
+                reject(error)
+            }
+            console.log(value)
+        })
+    })
+}
+
+fs.promises.readFile('.gitignore', 'utf-8')
+.then((value => console.log(value)))
+
+readFileInPromise('.gitignore').then((value => console.log(value)))
