751735969d2df44774ecf6dfe3ac8a8410e85839
Connor <kor.connor@gmail.com>
2022-07-21 00:15:36 +0900

    promise - reject

--- src/main.js
+++ src/main.js
@@ -5,4 +5,11 @@
 
 new Promise((resolve, reject) => {
     console.log('Inside promise')
+    reject(new Error('First reject'))
+    resolve('First resolve')
+}).then(value => {
+    console.log('Inside first then')
+    console.log('value', value)
+}).catch(error => {
+    console.log('error', error)
 })
