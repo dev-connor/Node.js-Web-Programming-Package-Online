4a254dad502c6eb6da813366f2e155de8ccdb4ed
Connor <kor.connor@gmail.com>
2022-07-21 23:04:03 +0900

    allSettled

--- src/main.js
+++ src/main.js
@@ -2,6 +2,32 @@
 
 require('core-js')
 
-const original = 'abcabc123'
-const changed = original.replaceAll('abc', '123')
-console.log(changed)
+/**
+ * 
+ * @param {number} duration 
+ * @returns 
+ */
+function sleep(duration) {
+    return new Promise((resolve) => {
+        console.log('sleep start')
+        setTimeout(() => {
+            console.log('sleep done', duration)
+            resolve(duration)
+        }, duration)
+    })
+}
+
+function alwaysReject() {
+    return new Promise((resolve, reject) => {
+        reject()
+    })
+}
+
+Promise.allSettled([
+    sleep(1000),
+    sleep(1500),
+    sleep(2000),
+    alwaysReject()
+]).then((value) => {
+    console.log('Promise.all done!', value)
+})
