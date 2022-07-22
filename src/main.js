67086f1f84a21082a7a4f32b0ca373dc7c1a2863
Connor <kor.connor@gmail.com>
2022-07-23 01:16:50 +0900

    build

--- src/main.js
+++ src/main.js
@@ -12,17 +12,4 @@ const objs = [
     },
 ]
 
-console.log(
-    objs.map((obj) => {
-        const {foo} = obj
-        if (foo) {
-            const { bar } = foo
-            if (bar) {
-                return bar.baz
-            }
-        }
-        return undefined
-    })
-)
-
 console.log(objs.map((obj) => obj.foo?.bar.baz))
