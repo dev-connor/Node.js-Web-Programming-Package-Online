5dfa8e805f4048ca849c639307f14bf8b89b44b7
Connor <kor.connor@gmail.com>
2022-08-01 16:41:30 +0900

    convention

--- src/main.js
+++ src/main.js
@@ -1,9 +1,28 @@
 // @ts-check
 
-const {log} = console
+const fs = require('fs')
 
-const a = require('./module')
-const b = require('./module')
-const c = require('./module')
+const FILENAME = 'src/main.js'
 
-console.log(a === b, b === c)
+// fs.readFile(FILENAME, 'utf-8', (err, result) => {
+//     if (err) {
+//         console.error(err)
+//     } else {
+//         console.log(result)
+//     }
+// })
+
+// try {
+//     const result = fs.readFileSync(FILENAME, 'utf-8')
+//     console.log(result)
+// } catch (error) {
+//     console.error(error)
+// }
+
+// promise-stype
+async function main() {
+    const result = await fs.promises.readFile(FILENAME, 'utf-8')
+    console.log(result)
+}
+
+main()
