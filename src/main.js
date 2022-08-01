4df9c27cf60ffdf5ed6f1fa83fc71f011cc215f6
Connor <kor.connor@gmail.com>
2022-08-01 16:46:56 +0900

    test 파일 작성 및 readFile

--- src/main.js
+++ src/main.js
@@ -2,27 +2,6 @@
 
 const fs = require('fs')
 
-const FILENAME = 'src/main.js'
+const result = fs.readFileSync('src/test')
 
-// fs.readFile(FILENAME, 'utf-8', (err, result) => {
-//     if (err) {
-//         console.error(err)
-//     } else {
-//         console.log(result)
-//     }
-// })
-
-// try {
-//     const result = fs.readFileSync(FILENAME, 'utf-8')
-//     console.log(result)
-// } catch (error) {
-//     console.error(error)
-// }
-
-// promise-stype
-async function main() {
-    const result = await fs.promises.readFile(FILENAME, 'utf-8')
-    console.log(result)
-}
-
-main()
+console.log(result)
