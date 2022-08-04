39389f2d813d352e93bcc97ab828a2c26ea2e166
Connor <kor.connor@gmail.com>
2022-08-04 22:57:38 +0900

    스트림

--- src/main.js
+++ src/main.js
@@ -2,25 +2,6 @@
 
 const fs = require('fs')
 
-const buf = Buffer.from([20, 23, 1, 5])
+const stream = fs.createReadStream('src/test')
 
-/**
- * 
- * @param {*} array 
- * @returns {number}
- */
-function readInt32LE(array) {
-    return (
-        array[0] 
-        + array[1] * 256 
-        + array[2] * 256 ** 2
-        + array[3] * 256 ** 3
-    )
-}
-
-const offset = 0
-
-const {log} = console
-
-log(`our function: `, readInt32LE(buf))
-log(`orig function: `, buf.readInt32LE(0))
+stream.pipe(process.stdout)
