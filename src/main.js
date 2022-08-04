12d34fe0c0aa98e19bbb110ea02ff5e088b3881e
Connor <kor.connor@gmail.com>
2022-08-04 22:54:23 +0900

    readInt32LE 함수 만들기

--- src/main.js
+++ src/main.js
@@ -2,6 +2,25 @@
 
 const fs = require('fs')
 
-const buf = Buffer.from([0, 1, 2, 3])
+const buf = Buffer.from([20, 23, 1, 5])
 
-console.log(Buffer.isBuffer(buf))
+/**
+ * 
+ * @param {*} array 
+ * @returns {number}
+ */
+function readInt32LE(array) {
+    return (
+        array[0] 
+        + array[1] * 256 
+        + array[2] * 256 ** 2
+        + array[3] * 256 ** 3
+    )
+}
+
+const offset = 0
+
+const {log} = console
+
+log(`our function: `, readInt32LE(buf))
+log(`orig function: `, buf.readInt32LE(0))
