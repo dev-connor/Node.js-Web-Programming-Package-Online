51bfac21b3855db39870ed3928d4a2be68c7b6e7
Connor <kor.connor@gmail.com>
2022-08-04 22:38:07 +0900

    Buffer.isBuffer

--- src/main.js
+++ src/main.js
@@ -2,17 +2,6 @@
 
 const fs = require('fs')
 
-const bufFromFile = fs.readFileSync('src/test')
-const buf = Buffer.from([97, 98, 99, 100, 101])
+const buf = Buffer.from([0, 1, 2, 3])
 
-const bufA = Buffer.from([0])
-const bufB = Buffer.from([3])
-const bufC = Buffer.from([2])
-const bufD = Buffer.from([6])
-
-const bufs = [bufA, bufB, bufC, bufD]
-// bufs.sort(Buffer.compare)
-bufs.sort((a, b) => a.compare(b))
-console.log(bufs)
-
-// console.log(buf.compare(bufFromFile))
+console.log(Buffer.isBuffer(buf))
