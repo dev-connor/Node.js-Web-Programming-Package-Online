bf38f7cc5810de0287dab2c770a4490f81b1cea2
Connor <kor.connor@gmail.com>
2022-08-06 00:52:32 +0900

    high water mark 50 까지 테스트

--- src/process-jsons.js
+++ src/process-jsons.js
@@ -4,45 +4,52 @@ const { log } = console
 
 const fs = require('fs')
 
-const rs = fs.createReadStream('local/jsons', {
-    encoding: 'utf-8',
-    highWaterMark: 20,
-})
-
-let totalSum = 0
-let accumulatedJsonStr = ''
-
-rs.on('data', (chunk) => {
-    log('Event: data', chunk)
-
+/**
+ * @param {number} highWaterMark
+ */
+function processJSON(highWaterMark) {
+    const rs = fs.createReadStream('local/jsons', {
+        encoding: 'utf-8',
+        highWaterMark,
+    })
     
-    if (typeof chunk !== 'string') {
-        return
-    }
-
-    accumulatedJsonStr += chunk
-
-    const lastNewlineIdx = accumulatedJsonStr.lastIndexOf('\n')
-
-    const jsonLinesStr = accumulatedJsonStr.substring(0, lastNewlineIdx)
-    accumulatedJsonStr = accumulatedJsonStr.substring(lastNewlineIdx)
+    let totalSum = 0
+    let accumulatedJsonStr = ''
     
-    totalSum += jsonLinesStr
-        .split('\n')
-        .map((jsonLine) => {
-            try {
-                return JSON.parse(jsonLine)
-            } catch (error) {
-                return undefined
-            }
-        })
-        .filter((json) => json)
-        .map((json) => json.data)
-        .reduce((sum, curr) => sum + curr, 0)  
-})
+    rs.on('data', (chunk) => {
+    
+        
+        if (typeof chunk !== 'string') {
+            return
+        }
+    
+        accumulatedJsonStr += chunk
+    
+        const lastNewlineIdx = accumulatedJsonStr.lastIndexOf('\n')
+    
+        const jsonLinesStr = accumulatedJsonStr.substring(0, lastNewlineIdx)
+        accumulatedJsonStr = accumulatedJsonStr.substring(lastNewlineIdx)
+        
+        totalSum += jsonLinesStr
+            .split('\n')
+            .map((jsonLine) => {
+                try {
+                    return JSON.parse(jsonLine)
+                } catch (error) {
+                    return undefined
+                }
+            })
+            .filter((json) => json)
+            .map((json) => json.data)
+            .reduce((sum, curr) => sum + curr, 0)  
+    })
+    
+    rs.on('end', () => {
+        log('Event: end')
+        log(`totalSum (highWatermark: ${highWaterMark})`, totalSum)
+    })
+}
 
-rs.on('end', () => {
-    log('Event: end')
-    log(`totalSum`, totalSum)
-})
-
+for (let watermark = 1; watermark < 50; watermark += 1) {
+    processJSON(watermark)
+}
