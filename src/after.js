b655c6e7f6ac4d508f16d6794908a811d454bfe9
Connor <kor.connor@gmail.com>
2022-08-06 22:14:19 +0900

    인용문을 배열로 출력

--- src/after.js
+++ src/after.js
@@ -14,26 +14,69 @@ const https = require('https')
 const GOTAPI_PREFIX = 'https://game-of-thrones-quotes.herokuapp.com/v1'
 
 /**
- * @returns {Promise<House[]}
+ * 
+ * @param {string} url 
+ * @returns {*}
  */
-async function getHouses() {
-    return new Promise(resolve => {
-        https.get(`${GOTAPI_PREFIX}/houses`, res => {
+async function getHttpsJSON(url) {
+    return new Promise((resolve, reject) => {
+        https.get(url, res => {
             let jsonStr = ''
             res.setEncoding('utf-8')
             res.on('data', (data) => {
                 jsonStr += data
             })
             res.on('end', () => {
-                resolve(JSON.parse(jsonStr))
+                try {
+                    const parsed = JSON.parse(jsonStr)
+                    resolve(parsed)
+                } catch {
+                    reject(
+                        new Error('The server response was not a valid JSON document.')
+                    )
+                } 
             })
         })
     })
+    
+}
+
+/**
+ * @returns {Promise<House[]}
+ */
+async function getHouses() {
+    return getHttpsJSON(`${GOTAPI_PREFIX}/houses`)
+}
+
+/**
+ * @param {string} quote
+ * @returns {string}
+ */
+function sanitize(quote) {
+    return quote.replace(/[^a-zA-Z0-9., ]/g, '')
+}
+
+/**
+ * 
+ * @param {string} slug 
+ * @returns {Promise<string>}
+ */
+async function getMergedQuotesOfCharacter(slug) {
+    const character = await getHttpsJSON(`${GOTAPI_PREFIX}/character/${slug}`)
+    return character[0].quotes.join(' ').replace(/[^a-zA-Z0-9., ]/g, '')
 }
 
 async function main() {
     const houses = await getHouses()
-    console.log(houses)
+
+    const results = await Promise.all(
+        houses
+        .map(house => 
+            house.members.map(member => getMergedQuotesOfCharacter(member.slug))
+        )
+        .flat()
+    )
+    console.log(results)
 }
 
 main()
