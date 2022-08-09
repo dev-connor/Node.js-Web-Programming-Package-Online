70ed4fbabb7678efd879930846d6791014c712c8
Connor <kor.connor@gmail.com>
2022-08-09 21:49:10 +0900

    닉네임 수정 추가

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
 // @ts-check
 
 const express = require('express')
-const fs = require('fs')
 
 const userRouter = express.Router()
 
 const app = express()
+app.use(express.json())
 
 const PORT = 5000
 
@@ -21,17 +21,31 @@ const USERS = {
 
 userRouter.param('id', (req, res, next, value) => {
     console.log(`id parameter`, value)
+    // @ts-ignore
+    req.user = USERS[value]
     next()
 })
 
 userRouter.get('/:id', (req, res) => {
     console.log('userRouter get ID')
-    res.send('User info with ID')
+    // @ts-ignore
+    res.send(req.user)
 })
 
 userRouter.post('/', (req, res) => {
     // Register user
     res.send('User registered.')
+})
+
+userRouter.post('/:id/nickname', (req, res) => {
+    // req.body: {"nickname": "bar"}
+    // @ts-ignore
+    const { user } = req
+    const { nickname } = req.body
+
+    user.nickname = nickname
+
+    res.send(`User nickname updated: ${nickname}`)
 })
 
 app.use('/users', userRouter)
