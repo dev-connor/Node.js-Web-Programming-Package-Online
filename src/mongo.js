79989c250c2687ec656547dfe243b658d69cbe7b
Connor <kor.connor@gmail.com>
2022-08-12 22:03:58 +0900

    쉘 변수로 비밀번호 저장

--- src/mongo.js
+++ src/mongo.js
@@ -2,12 +2,22 @@
 
 const { MongoClient, ServerApiVersion } = require('mongodb')
 
-const uri = "mongodb+srv://admin:<password>@cluster0.vrgvebb.mongodb.net/?retryWrites=true&w=majority"
+const uri = `mongodb+srv://admin:${process.env.MONGO_PASSWORD}@cluster0.vrgvebb.mongodb.net/?retryWrites=true&w=majority`
 const client = new MongoClient(uri, { 
     useNewUrlParser: true, 
     useUnifiedTopology: true, 
     // serverApi: ServerApiVersion.v1 
 })
+
+async function main() {
+    const c = await client.connect()
+    console.log(c)
+    console.log('OK!')
+
+    client.close()
+}
+
+main()
 
 // client.connect(err => {
 //   const collection = client.db("test").collection("devices")
