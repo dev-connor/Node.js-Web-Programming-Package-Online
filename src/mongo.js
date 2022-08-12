3b07f20bad0dec9de436e82af8b8a447f93a2429
Connor <kor.connor@gmail.com>
2022-08-12 22:26:43 +0900

    삭제, 삽입, 조회 API 추가

--- src/mongo.js
+++ src/mongo.js
@@ -1,26 +1,34 @@
 // @ts-check
 
-const { MongoClient, ServerApiVersion } = require('mongodb')
+const { MongoClient } = require('mongodb')
 
 const uri = `mongodb+srv://admin:${process.env.MONGO_PASSWORD}@cluster0.vrgvebb.mongodb.net/?retryWrites=true&w=majority`
 const client = new MongoClient(uri, { 
     useNewUrlParser: true, 
     useUnifiedTopology: true, 
-    // serverApi: ServerApiVersion.v1 
 })
 
 async function main() {
     const c = await client.connect()
-    console.log(c)
-    console.log('OK!')
 
-    client.close()
+    const users = client.db('fc21').collection('users')
+    
+    await users.deleteMany({})
+    await users.insertMany([
+        {
+            name: 'Foo',
+        },
+        {
+            name: 'Bar',
+        },
+        {
+            name: 'Baz',
+        },
+    ])
+    const cursor = users.find({})
+    await cursor.forEach(console.log)
+
+    await client.close()
 }
 
 main()
-
-// client.connect(err => {
-//   const collection = client.db("test").collection("devices")
-//   // perform actions on the collection object
-//   client.close()
-// })
