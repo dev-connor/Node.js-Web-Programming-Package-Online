a174fdd23627ec8790740a9d1c29e8403144c67e
Connor <kor.connor@gmail.com>
2022-08-13 17:50:37 +0900

    채팅서비스 전 client.js 호출하기 with mount

--- src/main.js
+++ src/main.js
@@ -3,17 +3,32 @@
 const Koa = require('koa')
 const Pug = require('koa-pug')
 const path = require('path')
+const route = require('koa-route')
+const serve = require('koa-static')
+const websockify = require('koa-websocket')
+const mount = require('koa-mount')
 
-const app = new Koa()
+const app = websockify(new Koa())
 
 // @ts-ignore
 new Pug({
     viewPath: path.resolve(__dirname, './views'),
     app, // Binding `ctx.render()`, equals to pug.use(app)
-  })
+})
+
+app.use(mount('/public', serve('src/public')))
 
 app.use(async (ctx) => {
   await ctx.render('main')
 })
+
+app.ws.use(
+    route.all('/test/:id', (ctx) => {
+        ctx.websocket.send('Hello World')
+        ctx.websocket.on('message', (message) => {
+            console.log(message)
+        })
+    })
+)
 
 app.listen(5000)
---
+++ src/public/client.js
@@ -0,0 +1,1 @@
+alert('Client.js loaded!')
