24cc2b18cf74cf2d6df59a9654cd7f39caa01c84
Connor <kor.connor@gmail.com>
2022-08-16 23:07:40 +0900

    이미지 API 호출

--- src/main.js
+++ src/main.js
@@ -1,2 +1,19 @@
 // @ts-check
 
+const { createApi } = require('unsplash-js')
+const { default: fetch} = require('node-fetch')
+const { default: convertLayerAtRulesToControlComments } = require('tailwindcss/lib/lib/convertLayerAtRulesToControlComments')
+
+
+const unsplash = createApi({
+  accessKey: process.env.UNSPLASH_API_ACCESS_KEY,
+  // @ts-ignore
+  fetch,
+})
+
+async function main() {
+    const result = await unsplash.search.getPhotos({ query: 'mountain'})
+    console.log(result)
+}
+
+main()
