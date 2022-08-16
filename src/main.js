5b3faf60d2be6b65e72bec2d16cb82b497d719c3
Connor <kor.connor@gmail.com>
2022-08-17 00:02:27 +0900

    dog 캐쉬 저장 성공 cloud 실패

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 // @ts-check
 
 const fs = require('fs')
+const path = require('path')
 const http = require('http')
 const { createApi } = require('unsplash-js')
 const { default: fetch} = require('node-fetch')
@@ -38,7 +39,31 @@ async function searchImage(query) {
 }
 
 /**
- * 
+ * @param {string} query
+ */
+async function getCachedImageOrSearchedImage(query) {
+    const imageFilePath = path.resolve(__dirname, `../images/${query}`)
+
+    if (fs.existsSync(imageFilePath)) {
+        return {
+            message: `Returning cached image: ${query}`,
+            stream: fs.createReadStream(imageFilePath)
+        }
+    }
+    
+    const result = await searchImage(query)
+    const resp = await fetch(result.url)
+    
+    resp.body.pipe(fs.createWriteStream(imageFilePath))
+    
+    return {
+        message: `Returning new image: ${query}`,
+        stream: fs.createReadStream(imageFilePath)
+    }
+}
+
+/**
+ * 이미지를 Unsplash 에서 검색하거나, 이미 있다면 캐시된 이미지를 리턴합니다.
  * @param {string} url 
  */
 function convertURLToQueryKeyword(url) {
@@ -57,9 +82,16 @@ const server = http.createServer((req, res) => {
         }
         
         const query = convertURLToQueryKeyword(req.url)
-        const result = await searchImage(query)
-        const resp = await fetch(result.url)
-        resp.body.pipe(res)
+        try {
+            const {message, stream} = await getCachedImageOrSearchedImage(query)
+            stream.pipe(res)
+            
+            console.log(message)
+        } catch {
+            res.statusCode = 400
+            res.end()
+            
+        }
 
     }
 
