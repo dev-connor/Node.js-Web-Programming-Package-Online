c26a6e67cd7e2b976c6c2340e9b286ab92f8663f
Connor <kor.connor@gmail.com>
2022-08-19 23:14:55 +0900

    커맨더의 커맨드 연습

--- src/main.js
+++ src/main.js
@@ -7,15 +7,20 @@ const { program } = require('commander')
 program.version('0.0.1')
 
 program
-  .option('-d, --debug', 'output extra debugging')
-  .option('-s, --small', 'small pizza size')
-  .option('-p, --pizza-type <type>', 'flavour of pizza')
+  .command('List-bugs')
+  .description('List issues with bug label')
+  .action(() => {
+    console.log('List bugs!');
+  })
+  
+program.parse()
 
-program.parse(process.argv)
 
-const options = program.opts()
-if (options.debug) console.log(options)
-console.log('pizza details:')
-if (options.small) console.log('- small pizza size')
-if (options.pizzaType) console.log(`- ${options.pizzaType}`)
+// program
+//   .command('start <service>', 'start named service')
+//   .command('stop [service]', 'stop named service, or all if no name supplied');
 
+// Command prepared separately.
+// Returns `this` for adding more commands.
+// program
+//   .addCommand(build.makeBuildCommand());
