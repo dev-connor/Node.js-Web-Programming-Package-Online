c96cdfb7f3c67ca2a961b729875bdc5f51e44a26
Connor <kor.connor@gmail.com>
2022-08-23 00:06:22 +0900

    add 명령어 만들기

--- src/main.js
+++ src/main.js
@@ -1,17 +1,41 @@
 // @ts-check
 
 const { Client } = require('pg')
-const client = new Client({
-    user: 'myuser',
-    password: 'mypass',
-    database: 'fc21',
-})
+const program = require('commander')
+const prompts = require('prompts')
 
-async function main() {
+async function connect() {
+    const client = new Client({
+        user: 'myuser',
+        password: 'mypass',
+        database: 'fc21',
+    })
     await client.connect()
-    const res = await client.query('SELECT $1::text as message', ['Hello world!'])
-    console.log(res.rows[0].message) // Hello world!
-    await client.end()
+    return client
 }
 
-main()
+program.command('add').action(async () => {
+    const client = await connect()
+    const userName = await prompts({
+        type: 'text',
+        name: 'userName',
+        message: 'Provide a user name to insert.',
+    })
+
+    const query = `INSERT INTO users (name) VALUES ('${userName.userName}')`
+    await client.query(query)
+    
+    await client.end()
+})
+
+program
+.command('remove')
+.action(async () => {
+    const client = await connect()
+    await client.end()
+})
+
+program.parseAsync()
+
+
+
