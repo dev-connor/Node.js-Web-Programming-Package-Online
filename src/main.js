b1fd57895690c19d34ff7bd8ed854781ec1913fe
Connor <kor.connor@gmail.com>
2022-08-23 23:33:42 +0900

    sequalize 시작하기

--- src/main.js
+++ src/main.js
@@ -1,59 +1,20 @@
 // @ts-check
 
-const { Client } = require('pg')
-const program = require('commander')
-const prompts = require('prompts')
+const { Sequelize } = require('sequelize')
 
-async function connect() {
-    const client = new Client({
-        user: 'myuser',
-        password: 'mypass',
+async function main() {
+    const sequelize = new Sequelize({
         database: 'fc21',
+        username: 'myuser',
+        password: 'mypass',
+        dialect: 'postgres',
+        host: 'localhost',
     })
-    await client.connect()
-    return client
+
+    await sequelize.authenticate()
+    await sequelize.close()
+
 }
 
-program.command('list').action(async () => {
-    const client = await connect()
-
-    const query = `SELECT * FROM users`
-    const result = await client.query(query)
-    console.log(result.rows)
-})
-
-program.command('add').action(async () => {
-    const client = await connect()
-    const userName = await prompts({
-        type: 'text',
-        name: 'userName',
-        message: 'Provide a user name to insert.',
-    })
-
-    const query = `INSERT INTO users (name) VALUES ($1::text)`
-    await client.query(query, [userName.userName])
-    
-    await client.end()
-})
-
-program
-.command('remove')
-.action(async () => {
-    const client = await connect()
-    const userName = await prompts({
-        type: 'text',
-        name: 'userName',
-        message: 'Provide a user name to delete.',
-    })
-
-    await client.query(`DELETE FROM users WHERE name = $1::text`, [
-        userName.userName,
-    ])
-    
-    await client.end()
-})
-
-program.parseAsync()
-
-
+main()
 
