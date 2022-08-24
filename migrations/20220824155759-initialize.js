7f75896acbd8ebbdfe520af586cb264d02ecce09
Connor <kor.connor@gmail.com>
2022-08-25 01:12:19 +0900

    migrations 해보기

--- migrations/20220824155759-initialize.js
+++ migrations/20220824155759-initialize.js
@@ -1,13 +1,18 @@
-'use strict';
+// @ts-check
 
 module.exports = {
+  /**
+   * 
+   * @param {import('sequelize').QueryInterface} queryInterface 
+   * @param {import('sequelize')} Sequelize 
+   */
   up: async (queryInterface, Sequelize) => {
     /**
      * Add altering commands here.
      *
      * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
      */
+    await queryInterface.createTable('users', { id: Sequelize.INTEGER });
   },
 
   down: async (queryInterface, Sequelize) => {
@@ -15,7 +20,7 @@ module.exports = {
      * Add reverting commands here.
      *
      * Example:
-     * await queryInterface.dropTable('users');
      */
+    await queryInterface.dropTable('users');
   }
 };
