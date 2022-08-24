ec8be7fb7f7f4beb09209abacdd51ffeafe82a1c
Connor <kor.connor@gmail.com>
2022-08-25 01:13:29 +0900

    npm run seq - - migration:generate --name add-cities

--- migrations/20220824155759-initialize.js
+++ migrations/20220824155759-initialize.js
@@ -7,20 +7,15 @@ module.exports = {
    * @param {import('sequelize')} Sequelize 
    */
   up: async (queryInterface, Sequelize) => {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     */
     await queryInterface.createTable('users', { id: Sequelize.INTEGER });
   },
-
+  
+  /**
+   * 
+   * @param {import('sequelize').QueryInterface} queryInterface 
+   * @param {import('sequelize')} Sequelize 
+   */
   down: async (queryInterface, Sequelize) => {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     */
     await queryInterface.dropTable('users');
   }
 };
