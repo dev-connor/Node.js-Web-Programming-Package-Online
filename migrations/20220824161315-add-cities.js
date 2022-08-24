92c0ee1175aec5ef49513a32b8bbe528cd5d0ce6
Connor <kor.connor@gmail.com>
2022-08-25 01:40:29 +0900

    두개의 seqelize 작성해보기

--- migrations/20220824161315-add-cities.js
+++ migrations/20220824161315-add-cities.js
@@ -1,21 +1,40 @@
-'use strict';
+// @ts-check
 
 module.exports = {
+  /**
+   * 
+   * @param {import('sequelize').QueryInterface} queryInterface 
+   * @param {import('sequelize')} Sequelize 
+   */
   up: async (queryInterface, Sequelize) => {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
-  },
+    await queryInterface.createTable('cities', {
+       id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+    })
 
-  down: async (queryInterface, Sequelize) => {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+    await queryInterface.addColumn('users', 'cityId', {
+      type: Sequelize.DataTypes.INTEGER,
+      references: {
+        model: 'cities',
+        key: 'id',
+      },
+    })
+  },
+  
+  /**
+   * 
+   * @param {import('sequelize').QueryInterface} queryInterface 
+   * @param {import('sequelize')} Sequelize 
+   */
+    down: async (queryInterface, Sequelize) => {
+    await queryInterface.removeColumn('users', 'cityId')
+    await queryInterface.dropTable('cities');
   }
-};
+}
