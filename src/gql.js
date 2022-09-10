3fa178e4b3c8123ffcf81c6fbf4c9dbfda68941f
Connor <kor.connor@gmail.com>
2022-09-10 20:29:18 +0900

    n

--- src/gql.js
+++ src/gql.js
@@ -69,6 +69,7 @@ const resolvers = {
     // ],
   })
   
+  
   // The `listen` method launches a web server.
   server.listen(5000).then(({ url }) => {
     console.log(`🚀  Server ready at ${url}`);
